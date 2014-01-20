643d83ec595f772d4992a2ed6b12c1fbf287829e
perry <perry@raptor2>
2014-01-20 02:40:14 -0800

    ex15 extra credit stuff

---
+++ ex15_1.rb
@@ -0,0 +1,24 @@
+# Get the first argument (filename for input file) from the builtin ARGV array
+# Uses [ARRAY].first array method
+filename = ARGV.first
+
+prompt = "> "
+# Get file handle, store in variable txt
+txt = File.open(filename)
+
+# print out the filename with string interpolation
+puts "Here's your file: #{filename}"
+# read file until EOF, the read method returns a string
+puts txt.read()
+
+puts "I'll also ask you to type it again:"
+print prompt  # Great use and construction of a prompt character
+# print doesn't end with newline like the puts command
+
+# Take input from user, chomp to remove newline from input string
+file_again = STDIN.gets
+
+# Same as first part, open file and read and print
+txt_again = File.open(file_again)
+
+puts txt_again.read()
